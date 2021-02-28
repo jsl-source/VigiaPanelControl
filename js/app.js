3d2595b55a0bfb498dad21c14d7e541cd7145315
jsl <jhonsmithluis@gmail.com>
2021-02-27 22:20:35 -0500

    Se corrige app.js por error de escritura se borra indez.zip

--- js/app.js
+++ js/app.js
@@ -3,6 +3,29 @@ window.onresize = function(event) {
     if(ventana>=859){
         const menu__items = document.querySelector('.menu__items');
         menu__items.classList.remove('show');
+        addEventListener('scroll', () => {
+
+            let ancho=window.screen.width;
+        
+            console.log(ancho);
+        
+            let scr=window.scrollY;
+            if(ancho>=859){
+        
+                if(scr<20){
+                    document.getElementById('cambiacolor').style.backgroundColor = 'transparent';
+                    document.getElementById('cambiacolor').style.transition = 'background-color 1s ease';
+                    document.getElementById('cambiacolor').style.boxShadow = 'none';
+                }else{
+                    document.getElementById('cambiacolor').style.backgroundColor = '#ffffff';
+                    document.getElementById('cambiacolor').style.transition = 'background-color 1s ease';
+                    document.getElementById('cambiacolor').style.boxShadow = '5px 0 10px rgba(0, 0, 0, .6)';
+                }
+            
+            }
+            
+        
+        })
     }
 };
 
@@ -22,27 +45,5 @@ addEventListener('DOMContentLoaded' ,()=>{
 
 })
 
-addEventListener('scroll', () => {
 
-    let ancho=screen.width;
-
-    console.log(ancho);
-
-    let scr=window.scrollY;
-    if(ancho>=859){
-
-        if(scr<10){
-            document.getElementById('cambiacolor').style.backgroundColor = 'transparent';
-            document.getElementById('cambiacolor').style.transition = 'background-color 1s ease';
-            document.getElementById('cambiacolor').style.boxShadow = 'none';
-        }else{
-            document.getElementById('cambiacolor').style.backgroundColor = '#ffffff';
-            document.getElementById('cambiacolor').style.transition = 'background-color 1s ease';
-            document.getElementById('cambiacolor').style.boxShadow = '5px 0 10px rgba(0, 0, 0, .6)';
-        }
-    
-    }
-    
-
-})
 
